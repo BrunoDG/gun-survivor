665ff486161fd177482919a645d754c1955a9395
Bruno Dias <brunodg.dev@gmail.com>
2025-08-03 18:55:43 -0300

    Implemented character movement

--- player/player.gd
+++ player/player.gd
@@ -1,13 +1,35 @@
 extends CharacterBody3D
 
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= event.relative.x * 0.5
 		%Camera3D.rotation_degrees.x -= event.relative.y * 0.2
 		%Camera3D.rotation_degrees.x = clamp(
-			%Camera3D.rotation_degrees.x, -80.0, 80.0
+			%Camera3D.rotation_degrees.x, -70.0, 70.0
 		)
-		# %Camera3D is the same as get_node("Camera3D") or $Camera3D
-		# %gun_model is the same as get_node("Camera3D/gun_model") or $Camera3D/gun_model
+		# %Camera3D is the same as get_node("Camera3D") or $Camera3D,
+		# but this uses an unique name, instead of using the node's
+		# full path to reference it.
+	
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(_delta):
+	const SPEED = 5.5
+	
+	var input_direction_2D = Input.get_vector(
+		"move_left", "move_right", "move_forward", "move_back"
+	)
+	var input_direction_3D = Vector3(
+		input_direction_2D.x, 0.0, input_direction_2D.y
+	)
+	var direction = transform.basis * input_direction_3D
+	
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+	
+	move_and_slide()
